9d6372cb0913d683bb61052dceefc2a8caf1e065
sujitha_saranam <mfsi.sujithas@gmail.com>
2022-12-23 14:41:35 +0530

    last commit

--- weatherdisplayer/urls.py
+++ weatherdisplayer/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path,include
+from django.conf.urls import url
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
